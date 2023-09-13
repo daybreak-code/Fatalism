34e8574b915ac055efcac7c19f278ebe27ce01ef
3358115621@qq.com <3358115621@qq.com>
2023-09-13 08:29:45 +0800

    enable springboot

--- fatalism-repayment-service/src/main/java/cn/daycode/fatalism/repayment/RepaymentApp.java
+++ fatalism-repayment-service/src/main/java/cn/daycode/fatalism/repayment/RepaymentApp.java
@@ -1,13 +1,13 @@
-package cn.daycode.fatalism.consumer;
+package cn.daycode.fatalism.repayment;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RepaymentApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(RepaymentApp.class);
     }
 }
