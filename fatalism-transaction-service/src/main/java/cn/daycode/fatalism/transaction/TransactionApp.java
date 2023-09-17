efffdee64f848f3ec04e8e8d783b6470a333e7f4
3358115621@qq.com <3358115621@qq.com>
2023-09-18 07:27:45 +0800

    add transaction API

---
+++ fatalism-transaction-service/src/main/java/cn/daycode/fatalism/transaction/TransactionApp.java
@@ -0,0 +1,13 @@
+package cn.daycode.fatalism.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(TransactionApp.class);
+    }
+}
