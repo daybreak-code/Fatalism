6980586687d676ce1f8b2596b7e818ee84ed199f
3358115621@qq.com <3358115621@qq.com>
2023-09-26 08:19:28 +0800

    refactor AccountApp

--- fatalism-account-service/src/main/java/cn/daycode/fatalism/account/AccountApp.java
+++ fatalism-account-service/src/main/java/cn/daycode/fatalism/account/AccountApp.java
@@ -1,13 +1,13 @@
 package cn.daycode.fatalism.account;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AccountApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(AccountApp.class);
     }
 }
