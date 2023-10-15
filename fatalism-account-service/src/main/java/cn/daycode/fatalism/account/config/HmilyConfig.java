10970d9b816a614699f5cc36487ee876e03fc89c
3358115621@qq.com <3358115621@qq.com>
2023-10-15 11:51:57 +0800

    refactor code structure

--- fatalism-account-service/src/main/java/cn/daycode/fatalism/account/config/HmilyConfig.java
+++ fatalism-account-service/src/main/java/cn/daycode/fatalism/account/config/HmilyConfig.java
@@ -13,19 +13,4 @@ public class HmilyConfig {
     @Autowired
     private Environment env;
 
- /*   @Bean
-    public HmilyTransactionBootstrap hmilyTransactionBootstrap(HmilyInitService hmilyInitService){
-        HmilyTransactionBootstrap hmilyTransactionBootstrap = new HmilyTransactionBootstrap(hmilyInitService);
-        hmilyTransactionBootstrap.setSerializer(env.getProperty("org.dromara.hmily.serializer"));
-        hmilyTransactionBootstrap.setRetryMax(Integer.parseInt(env.getProperty("org.dromara.hmily.retryMax")));
-        hmilyTransactionBootstrap.setRepositorySupport(env.getProperty("org.dromara.hmily.repositorySupport"));
-        hmilyTransactionBootstrap.setStarted(Boolean.parseBoolean(env.getProperty("org.dromara.hmily.started")));
-        HmilyDbConfig hmilyDbConfig = new HmilyDbConfig();
-        hmilyDbConfig.setDriverClassName(env.getProperty("org.dromara.hmily.hmilyDbConfig.driverClassName"));
-        hmilyDbConfig.setUrl(env.getProperty("org.dromara.hmily.hmilyDbConfig.url"));
-        hmilyDbConfig.setUsername(env.getProperty("org.dromara.hmily.hmilyDbConfig.username"));
-        hmilyDbConfig.setPassword(env.getProperty("org.dromara.hmily.hmilyDbConfig.password"));
-        hmilyTransactionBootstrap.setHmilyDbConfig(hmilyDbConfig);
-        return hmilyTransactionBootstrap;
-    }*/
 }
