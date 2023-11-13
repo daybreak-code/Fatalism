a4eb8ca619a198d14efabbcadb5ddf0859d70595
3358115621@qq.com <3358115621@qq.com>
2023-11-13 08:08:59 +0800

    code refactor

--- fatalism-depository-agent-service/src/main/java/cn/daycode/fatalism/depository/DepositoryAgentApp.java
+++ fatalism-depository-agent-service/src/main/java/cn/daycode/fatalism/depository/DepositoryAgentApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DepositoryApp
+public class DepositoryAgentApp
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(DepositoryApp.class);
+        SpringApplication.run(DepositoryAgentApp.class);
     }
 }
--- fatalism-depository-agent-service/src/test/java/cn/daycode/fatalism/DepositoryAgentAppTest.java
+++ fatalism-depository-agent-service/src/test/java/cn/daycode/fatalism/DepositoryAgentAppTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DepositoryAppTest
+public class DepositoryAgentAppTest
 {
 
     @Test
