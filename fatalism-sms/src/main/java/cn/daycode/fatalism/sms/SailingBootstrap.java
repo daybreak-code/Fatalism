d7f22bdd17de4270ba25a8ee74d2022e354bfb3d
3358115621@qq.com <3358115621@qq.com>
2023-11-20 08:36:18 +0800

    init SMS repo

--- fatalism-sms/src/main/java/cn/daycode/fatalism/sms/SailingBootstrap.java
+++ fatalism-sms/src/main/java/cn/daycode/fatalism/sms/SailingBootstrap.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SailingBootstrap {
 
     public static void main(String[] args) {
-//        org.springframework.util.comparator.InvertibleComparator
         SpringApplication.run(SailingBootstrap.class, args);
     }
 
