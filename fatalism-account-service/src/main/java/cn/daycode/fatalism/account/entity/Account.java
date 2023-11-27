aecc15eaa737b245464e99503025b7db86fb6104
3358115621@qq.com <3358115621@qq.com>
2023-11-27 08:30:23 +0800

    code refactor

--- fatalism-account-service/src/main/java/cn/daycode/fatalism/account/entity/Account.java
+++ fatalism-account-service/src/main/java/cn/daycode/fatalism/account/entity/Account.java
@@ -2,80 +2,38 @@ package cn.daycode.fatalism.account.entity;
 
 import java.io.Serializable;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("account")
 public class Account implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @TableId("ID")
     private Long id;
 
+    @TableField("USERNAME")
     private String username;
 
+    @TableField("MOBILE")
     private String mobile;
 
+    @TableField("PASSWORD")
     private String password;
 
+    @TableField("SALT")
     private String salt;
 
+    @TableField("STATUS")
     private Integer status;
 
+    /**
+     * domain(c：c side user；b：b side user)
+     */
+    @TableField("DOMAIN")
     private String domain;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getSalt() {
-        return salt;
-    }
-
-    public void setSalt(String salt) {
-        this.salt = salt;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
 }
